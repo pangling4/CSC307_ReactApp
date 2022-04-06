7b2c78363ddd50d678782d2529b7da400f3e7036
pangling4 <philipang515@gmail.com>
2022-04-06 13:20:51 -0700

    implement 201 http code

--- src/MyApp.js
+++ src/MyApp.js
@@ -45,11 +45,11 @@ function MyApp() {
 
   function updateList(person) {
     makePostCall(person).then( result => {
-      if (result && result.status === 200)
+      if (result && result.status === 201)
          setCharacters([...characters, person] );
       });
   }
-  
+
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
